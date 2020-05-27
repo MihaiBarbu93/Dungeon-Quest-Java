40cee08f6cc3dbcf72e506c5ea100070bcdfbe7b
AlexCatalin21 <alexandru.g2101@gmail.com>
2020-05-27 12:04:57 +0300

    Inventory implemented

--- src/main/java/com/codecool/quest/Main.java
+++ src/main/java/com/codecool/quest/Main.java
@@ -7,18 +7,23 @@ import com.codecool.quest.logic.MapLoader;
 import com.codecool.quest.logic.actors.Actor;
 import com.codecool.quest.logic.actors.Player;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 
 public class Main extends Application {
@@ -29,8 +34,11 @@ public class Main extends Application {
             map.getHeight() * Tiles.TILE_WIDTH);
     GraphicsContext context = canvas.getGraphicsContext2D();
     Label healthLabel = new Label();
+    Label attackLabel = new Label();
+    Label defenseLabel = new Label();
 
     static Button Pickup = new Button("Pick Up");
+
 
 
     public static void main(String[] args) {
@@ -39,14 +47,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        ObservableList<String> items= FXCollections.observableArrayList();
+        ListView<String> itemsList= new ListView<>(items);
+        itemsList.prefWidth(20);
+        itemsList.prefHeight(10);
         GridPane ui = new GridPane();
-        ui.setPrefWidth(200);
+        ui.setPrefWidth(300);
         ui.setPadding(new Insets(10));
-
-        ui.add(new Label("Health: "), 0, 0);
-        ui.add(healthLabel, 1, 0);
-
+        ui.setVgap(5);
+        ui.add(healthLabel, 0, 0);
+        ui.add(attackLabel, 0, 1);
+        ui.add(defenseLabel,0,2);
         ui.add(Pickup,0,5);
+        ui.add(new Label("Inventory: "), 0, 6);
+        ui.add(itemsList,0,7);
 
         BorderPane borderPane = new BorderPane();
 
@@ -68,10 +82,17 @@ public class Main extends Application {
                         for (int y = 0; y < map.getHeight(); y++) {
                             Cell cell = map.getCell(x, y);
                             if (cell.getTileName().equals("weapon") && cell.getActor() != null) {
+                                map.getPlayer().setAttack(map.getPlayer().getAttack()+5);
+                                refresh();
+                                items.add(cell.getTileName());
                                 cell.setType(CellType.FLOOR);
                             } else if (cell.getTileName().equals("key") && cell.getActor() != null ) {
+                                items.add(cell.getTileName());
                                 cell.setType(CellType.FLOOR);
                             } else if (cell.getTileName().equals("helmet") && cell.getActor() != null) {
+                                map.getPlayer().setDefense(map.getPlayer().getDefense()+2);
+                                refresh();
+                                items.add(cell.getTileName());
                                 cell.setType(CellType.FLOOR);
                             }
                         }
@@ -116,7 +137,9 @@ public class Main extends Application {
                 }
             }
         }
-        healthLabel.setText("" + map.getPlayer().getHealth());
+        healthLabel.setText("Health:" + map.getPlayer().getHealth());
+        attackLabel.setText("Attack:" + map.getPlayer().getAttack());
+        defenseLabel.setText("Defense:" + map.getPlayer().getDefense());
     }
     private void attack(){
     }
--- src/main/java/com/codecool/quest/logic/actors/Actor.java
+++ src/main/java/com/codecool/quest/logic/actors/Actor.java
@@ -7,6 +7,8 @@ import com.codecool.quest.logic.Drawable;
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
+    private int attack=2;
+    private int defense =0;
 
     public Actor(Cell cell) {
         this.cell = cell;
@@ -26,6 +28,21 @@ public abstract class Actor implements Drawable {
         return health;
     }
 
+    public int getDefense() {
+        return defense;
+    }
+    public int getAttack() {
+        return attack;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
     public Cell getCell() {
         return cell;
     }
